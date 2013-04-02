fd7aad89f94a80a69e04e739c09e9c84e98536ef
Mike Taylor <github@whatsmycut.com>
2013-04-02 02:09:16 -0700

    initial code checkin

---
+++ README.md
@@ -0,0 +1,15 @@
+# MTEmberApp
+
+MTEmberApp was created using the ember.js and ember_data.js Javascript MVC client-side framework libraries as an example of rapid development for dynamic web apps. It also uses the Twitter Bootrap CSS and Javascript libraries for the UI look and feel, as well as moment.js for date formatting, and showdown.js for HTML markdown.
+
+More on Ember.js can be found at: [ember.js](http://emberjs.com/)
+More on the [Ember.js Starter Kit] can be found at: [Ember.js Starter Kit](https://github.com/emberjs/starter-kit/downloads)
+More on Twitter Bootstrap can be found at: [bootstrap.js](http://twitter.github.com/bootstrap/)
+More on moment.js can be found at: [moment.js](http://momentjs.com/)
+More on showdown.js can be found at: [showdown.js](https://github.com/coreyti/showdown)
+
+## Revision History
+###r1: 
+Initial checkin of files to GitHub. 
+Basic app functionality in place.
+TODO: Create REST API
---
+++ js/app.js
@@ -0,0 +1,52 @@
+MTEmberApp = Ember.Application.create();
+
+MTEmberApp.Store = DS.Store.extend({
+	revision: 12,
+	adapter: "DS.FixtureAdapter"
+});
+
+MTEmberApp.Router.map(function() {
+  // put your routes here
+  // You should use this.resource for URLs that represent a noun, 
+  // and this.route for URLs that represent adjectives or verbs modifying those nouns.
+    this.resource('about');
+    this.resource('contacts');
+});
+
+// App.ApplicationController = Ember.Controller.extend({
+// 	title: "default";
+// });
+
+MTEmberApp.IndexRoute = Ember.Route.extend({
+  setupController: function(controller) {
+	title = setTitle("Mike Taylor Design.com");
+    controller.set('title', title);
+    controller.set('content', ['red', 'yellow', 'blue']);
+  }
+});
+
+MTEmberApp.AboutRoute = Ember.Route.extend({
+    model: function() {
+        return ['about us'];
+    }
+});
+
+MTEmberApp.ContactsRoute = Ember.Route.extend({
+    model: function() {
+        return ['hello there'];
+    }
+});
+
+Ember.Handlebars.registerBoundHelper('date', function(date) {
+	return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+Ember.Handlebars.registerBoundHelper('markdown', function(str) {
+	return Ember.Handlebars.safeString(showdown.makeHtml(str));
+});
+
+var setTitle = function(title) {
+	window.document.title = title;
+	return title;
+}
